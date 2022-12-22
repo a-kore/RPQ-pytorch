bc7a88838f3858603d4e62a57a7eda9e4ec6bb4a
akore <akore0x5f@gmail.com>
2022-12-22 11:11:36 -0500

    bump up to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'RPQ-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.1',
+  version = '0.0.2',
   license='MIT',
   description = 'Reverse Product Quantization (RPQ) of weights to reduce static memory usage.',
   author = 'Ali Kore',
